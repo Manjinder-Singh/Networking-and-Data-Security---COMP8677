2b4e3328c675ff1b5d4a21017a261147c3c5dcdb
Manjinder Singh <rahul.manjinder@yahoo.com>
2023-12-17 02:09:09 -0500

    2nd commit

--- Class Content/Week - 4/Sniff_Spoof/Sniff_Spoof/Sniff_Spoof/icmp_spoof.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from scapy.all import *
-
-print("SENDING SPOOFED ICMP PACKET.........")
-ip = IP(src="10.9.0.5", dst="10.10.10.10") 
-icmp = ICMP()
-pkt = ip/icmp/"fdafdalfhal"
-pkt.show2()
-send(pkt)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Networking-and-Data-Security---COMP8677
-COMP8677-3-R-2023F|Networking and Data Security (Monday)
